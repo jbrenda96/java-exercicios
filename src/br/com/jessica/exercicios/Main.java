73ce921030a95bf43c5d7dbf440220f71c460c42
Jessica Brenda <jessica.apec@gmail.com>
2026-02-01 16:22:57 -0300

    cria Ex01 aula01

--- src/br/com/jessica/exercicios/Main.java
+++ src/br/com/jessica/exercicios/Main.java
@@ -1,7 +1,4 @@
 package br.com.jessica.exercicios;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Primeiro commit no GitHub 🚀");
-    }
 }
---
+++ src/br/com/jessica/exercicios/aula01/Ex01.java
@@ -0,0 +1,7 @@
+package br.com.jessica.exercicios.aula01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println("Aula 01 - Ex01 rodando!");
+    }
+}
